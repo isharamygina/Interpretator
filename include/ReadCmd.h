9ddab474a490a6177b726afa84bb55df588ca6b7
Petr-Zaznobin <pitzaznobin@gmail.com>
2024-04-23 21:26:45 +0300

    Edited ReadCmd.h, ReadCmd.cpp, main.cpp

--- include/ReadCmd.h
+++ include/ReadCmd.h
@@ -8,11 +8,15 @@
 class ReadCmd{
 private:
     string cmd;
+    map<string, int> vals;
+    map<string, int> funcs;
 public:
     ReadCmd(string cmd_); //конструктор
     ~ReadCmd(); //деструктор
     void Parsing(); //разбор строки на составляющие (функции, переменные)
-    void myFor(int start, int end, int step); //обработка for
+    void myValue(); //обработка объявления переменной
+    void changeValue(); //обработка изменения переменной
+    void myFor(); //обработка for
     void myWhile();//обработка while
     void myIf(); // обработка if-else_if-else
     void findfun(); //Возвращает определенный флаг если while, for, if, else, else if, print, input,  нет
--- src/ReadCmd.cpp
+++ src/ReadCmd.cpp
@@ -1,2 +1,48 @@
 #include "ReadCmd.h"
+#define Cmd ReadCmd
+using namespace std;
 
+Cmd::ReadCmd(string cmd_) {
+    cmd = cmd_;
+    map<string, int> funcs = {{"for",     1},
+                              {"while",   1},
+                              {"if",      1},
+                              {"else",    1},
+                              {"input",   1},
+                              {"print",   1}};
+}
+
+Cmd::~ReadCmd() {
+    cmd = {};
+    vals.clear();
+    funcs.clear();
+}
+
+void Cmd::Parsing() {
+    int i = 0;
+    string word = "";
+    while(i < cmd.size()){
+        int j = i;
+        while (j != ' '){
+            word.push_back(cmd[j]);
+            j++;
+        }
+        if(funcs.count(word)==1){
+            switch (word[0]) {
+                case 'i' || 'e':
+                   myIf();
+                   break;
+                case 'f':
+                    myFor();
+                    break;
+                case 'w':
+                    myWhile();
+                    break;
+            }
+        }
+        else{
+            myValue();
+        }
+        i+=j;
+    }
+}
