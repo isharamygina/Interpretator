8ea6a32d8902a2c31ff8255742e89c792037b3af
Petr-Zaznobin <pitzaznobin@gmail.com>
2024-04-24 20:07:12 +0300

    Edited ReadCmd.h, ReadCmd.cpp, main.cpp

--- include/ReadCmd.h
+++ include/ReadCmd.h
@@ -8,17 +8,23 @@
 class ReadCmd{
 private:
     string cmd;
-    map<string, int> vals;
-    map<string, int> funcs;
+    map<string, double> vals; //мапа переменных//может надо перекинуть в ReadBlock и затем унаследовать сюда????
+    map<string, int> dataTypes;//мапа типов данных
+    map<string, int> funcs; //мапа доступных функций
+    map<char, int> ops; //мапа доступных операций
 public:
     ReadCmd(string cmd_); //конструктор
     ~ReadCmd(); //деструктор
     void Parsing(); //разбор строки на составляющие (функции, переменные)
-    void myValue(); //обработка объявления переменной
+    void myValue(string type); //обработка объявления переменной
     void changeValue(); //обработка изменения переменной
     void myFor(); //обработка for
     void myWhile();//обработка while
     void myIf(); // обработка if-else_if-else
     void findfun(); //Возвращает определенный флаг если while, for, if, else, else if, print, input,  нет
+    int stringToInt(string data); //преобразование строки в int
+    double stringToDouble(string data); //преобразование строки в double
+    float stringToFloat(string data); //преобразование строки в float
+    char stringToChar(string data); //преобразование строки в char
     friend ostream &operator<<(ostream &ostr, const ReadCmd &rc); //вывод строки (при исключениях) (можно выводить и номер строки)
 };
--- src/ReadCmd.cpp
+++ src/ReadCmd.cpp
@@ -1,48 +1,149 @@
 #include "ReadCmd.h"
-#define Cmd ReadCmd
 using namespace std;
 
-Cmd::ReadCmd(string cmd_) {
+ReadCmd::ReadCmd(string cmd_) {
     cmd = cmd_;
-    map<string, int> funcs = {{"for",     1},
-                              {"while",   1},
-                              {"if",      1},
-                              {"else",    1},
-                              {"input",   1},
-                              {"print",   1}};
+    map<string, int> dataTypes = {{"int",1},
+                              {"double", 1},
+                              {"float",  1},
+                              {"char",   1}};
+    map<string, int> funcs = {{"for",   1},
+                              {"while", 1},
+                              {"if",    1},
+                              {"else",  1},
+                              {"input", 1},
+                              {"print", 1}};
+    map<char, int> ops = {{'+', 1},
+                          {'-', 1},
+                          {'*', 1},
+                          {'/', 1},
+                          {'%', 1}};
 }
 
-Cmd::~ReadCmd() {
+ReadCmd::~ReadCmd() {
     cmd = {};
     vals.clear();
     funcs.clear();
 }
 
-void Cmd::Parsing() {
+void ReadCmd::Parsing() {
     int i = 0;
     string word = "";
-    while(i < cmd.size()){
-        int j = i;
-        while (j != ' '){
-            word.push_back(cmd[j]);
-            j++;
+    while (cmd[i]!= ' ' || cmd[i]!='(') {
+        word.push_back(cmd[i]);
+        i ++;
+    }
+    if (funcs.count(word) == 1) {
+        switch (word[i]) {
+            case 'i' || 'e':
+                myIf();
+                break;
+            case 'f':
+                myFor();
+                break;
+            case 'w':
+                myWhile();
+                break;
         }
-        if(funcs.count(word)==1){
-            switch (word[0]) {
-                case 'i' || 'e':
-                   myIf();
-                   break;
-                case 'f':
-                    myFor();
-                    break;
-                case 'w':
-                    myWhile();
-                    break;
-            }
+    } else {
+        myValue(word);
+    }
+}
+
+void ReadCmd::myValue(string type) {
+    int i = 0;
+    string name= "";
+    string data= "";
+    if (dataTypes.count(type) == 1){
+        switch (type[0]) {
+            case 'i':
+                for (int i = type.size()+1; cmd[i]!=' '; i++){
+                    name.push_back(cmd[i]);
+                }
+                while(cmd[i]!= '='){
+                    i++;
+                    if (i > cmd.size()){
+                        throw length_error("No symbol = in assignment");
+                    }
+                }
+                i++;
+                for(i;cmd[i]!=';'; i++){
+                    data.push_back(cmd[i]);
+                }
+                int intData = stringToInt(data);
+                vals.insert(pair<string, int>(name, intData));
+                break;
+
+            case 'd':
+                for (int i = type.size()+1; cmd[i]!=' '; i++){
+                    name.push_back(cmd[i]);
+                }
+                while(cmd[i]!= '='){
+                    i++;
+                    if (i > cmd.size()){
+                        throw length_error("No symbol = in assignment");
+                    }
+                }
+                i++;
+                for(i;cmd[i]!=';'; i++){
+                    data.push_back(cmd[i]);
+                }
+                int doubleData = stringToDouble(data);
+                vals.insert(pair<string, int>(name, doubleData));
+                break;
+
+            case 'f':
+                for (int i = type.size()+1; cmd[i]!=' '; i++){
+                    name.push_back(cmd[i]);
+                }
+                while(cmd[i]!= '='){
+                    i++;
+                    if (i > cmd.size()){
+                        throw length_error("No symbol = in assignment");
+                    }
+                }
+                i++;
+                for(i;cmd[i]!=';'; i++){
+                    data.push_back(cmd[i]);
+                }
+                int floatData = stringToFloat(data);
+                vals.insert(pair<string, int>(name, floatData));
+                break;
+
+            case 'c':
+                for (int i = type.size()+1; cmd[i]!=' '; i++){
+                    name.push_back(cmd[i]);
+                }
+                while(cmd[i]!= '='){
+                    i++;
+                    if (i > cmd.size()){
+                        throw length_error("No symbol = in assignment");
+                    }
+                }
+                i++;
+                for(i;cmd[i]!=';'; i++){
+                    data.push_back(cmd[i]);
+                }
+                int charData = stringToChar(data);
+                vals.insert(pair<string, int>(name, charData));
+                break;
+
+            default: //значит это операция над переменной
+
+                for (int i; cmd[i]!= ' '; i++){
+                    name.push_back(cmd[i]);
+                }
+                while(cmd[i]!= '=') {
+                    i++;
+                    if (i > cmd.size()) {
+                        throw length_error("No symbol = in operation");
+                    }
+                }
+                i++;
+                for(i; ops.count(cmd[i])==0; i++){
+
+                }
+                break;
         }
-        else{
-            myValue();
-        }
-        i+=j;
     }
 }
